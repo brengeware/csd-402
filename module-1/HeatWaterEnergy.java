be01f21853359397dedecb2ec7c5334a29fa5956
chromafella <brennan.cheatwood2003@gmail.com>
2025-12-04 16:05:16 -0500

    Uploading M1Program

---
+++ module-1/HeatWaterEnergy.java
@@ -0,0 +1,33 @@
+//Brennan Cheatwood
+//CSD-402 - M1 Programming Assignment
+
+import java.util.Scanner;
+
+public class HeatWaterEnergy {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//input
+		System.out.print("Enter the amount of water in kilograms: ");
+		double waterMass = input.nextDouble();
+		
+		System.out.print("Enter the initial Temperature in Celsius: ");
+		double initialTemperature = input.nextDouble();
+		
+		System.out.print("Enter the final Temperature in Celsius: ");
+		double finalTemperature = input.nextDouble();
+		
+		//water constant (formula)
+		final double SPECIFIC_HEAT_WATER = 4184.0;
+		
+		//calculate energy Q
+		double Q = waterMass * (finalTemperature - initialTemperature) * SPECIFIC_HEAT_WATER;
+		
+		//show result to user
+		System.out.println("The energy needed is " + Q + " Joules.");
+		
+		input.close();
+		
+	}
+}
+
